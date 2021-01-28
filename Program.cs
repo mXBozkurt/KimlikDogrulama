a272174890e2a0b99935f82ebfe42b476eb6bc42
mX <mX@DESKTOP-JIK5ITT>
2021-01-29 00:24:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceAndAbstractDemo.Abstract;
+using InterfaceAndAbstractDemo.Adapters;
+using InterfaceAndAbstractDemo.Concrete;
+using InterfaceAndAbstractDemo.Entities;
+
+namespace InterfaceAndAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1999, 1, 5), FirstName = "", LastName = "", NationalityId = "" });
+            Console.ReadLine();
+        }
+    }
+}
